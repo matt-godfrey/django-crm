836b83cb3da3491edea61f6dc9a77785974e322b
Matt Godfrey <godfrey.m1992@gmail.com>
2024-09-25 22:57:07 -0400

    initial commit

---
+++ leads/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+# from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+# models are a representation of the database schema
+
+# User = get_user_model() -> create custom User class
+
+class User(AbstractUser):
+    pass
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Lead(models.Model):
+
+    # SOURCE_CHOICES = (
+    #     ('YouTube', 'YouTube'),
+    #     ('Google', 'Google'),
+    #     ('NewsLetter', 'Newsletter')
+    # )
+
+    # phoned = models.BooleanField(default=False)
+    # source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # special_files = models.FileField(blank=True, null=True)
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
