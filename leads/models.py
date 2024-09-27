6c36f697885eba9e114af1ce8f2252ba43bb09e6
Matt Godfrey <godfrey.m1992@gmail.com>
2024-09-26 22:52:44 -0400

    experimented with views

--- leads/models.py
+++ leads/models.py
@@ -11,6 +11,9 @@ class User(AbstractUser):
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.email
+
 class Lead(models.Model):
 
     # SOURCE_CHOICES = (
@@ -28,3 +31,6 @@ class Lead(models.Model):
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
